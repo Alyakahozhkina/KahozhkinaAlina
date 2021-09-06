13166e88d5208b6b4f1c0320c36e3559c18138e5
Alina Finko <alya.finko@gmail.com>
2021-09-06 16:53:33 +0300

    HW36

--- Home Work/HomeWork36/js/main.js
+++ Home Work/HomeWork36/js/main.js
@@ -6,15 +6,18 @@
 jQuery(document).ready(function(){
     jQuery(".header__btn-order").click(function(){
         jQuery(".modal").fadeIn();
+        jQuery("body").addClass("fixed");
     });
 
     jQuery(".modal__close").click(function() {
         jQuery(".modal").fadeOut();
+        jQuery("body").removeClass("fixed");
     });
 
     jQuery(".modal").click(function(event){
         if(jQuery(event.target).closest(".modal__content").length === 0) {
         jQuery(this).fadeOut();
+        jQuery("body").removeClass("fixed");
         }
     });
 
